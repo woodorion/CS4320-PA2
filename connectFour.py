dff4a289950d11bdf6ebd5ed00c1b6f33aa49888
woodo <owood@miners.utep.edu>
2025-04-05 13:21:28 -0600

    Changed UR Algorithm to it's own method

--- connectFour.py
+++ connectFour.py
@@ -4,18 +4,52 @@
 import sys
 import random
 
-#Uniform Random Method
-def UR_Algorithm(currentPlayer, gameBoard):
-    allowedMoved = []
+#Uniform Random Method - takes in the current player and the game board
+#runs through board, till game is over (either win or draw)
+def UR_Algorithm(current_player, gameBoard):
+    if len(gameBoard) != 6:
+            raise ValueError("You need 6 rows for the board")
+    # Start array with nothing to check allowed moves; 
+    # if it is zero then its illegal
+    allowedMoves = []
     try:
         # Travel through the columns to check for allowed moves
-        allowedMoves = [col for col in range(7) if board[0][col] == 'O']    #If top row is full, column cannot be added to
+        allowedMoves = [col for col in range(7) if gameBoard[0][col] == 'O']    #If top row is full, column cannot be added to
         if not allowedMoves:
             raise ValueError("No valid moves at onset, board is already full")     #Check for errors if no allowed moves
     except Exception as e:
         print(f"Not able to travel through array: {e}")
         sys.exit(1)
-        
+
+    # Check for errors if there is an index issue
+    if not allowedMoves:
+        print("Index issue moves not allowed.")
+        sys.exit(1)
+    
+    # Use the UR algorithm 
+    while True:
+        allowedMoves = [col for col in range(7) if gameBoard[0][col] == 'O']    #check for allowed moves every move
+        if not allowedMoves:
+            print("Game Over: No Winner")
+            break
+        chosen_move = random.choice(allowedMoves) #Chooses random valid move 
+
+        #if selected column is not full (aka piece is dropped down correctly)
+        if dropPiece(gameBoard, chosen_move, current_player):
+            if winLogic(gameBoard, current_player):     #checks if there is a 4 in a row for the current player
+                print(f"FINAL Move Selected: {chosen_move + 1}")
+                print(f"Player {current_player} wins!")
+                printBoard(gameBoard)
+                break
+            print(f"Player {current_player} chooses column {chosen_move + 1}")
+            #printBoard(board)  #only used for testing purposes
+        else:
+            print(f"Error Coult not place piece in column {chosen_move + 1}")
+        #alternates players (always assumed R if current_player somehow isn't R or Y)
+        if current_player == 'R':
+            current_player = 'Y'
+        else:
+            current_player = "R"
 #Method to print board, for testing purposes
 def printBoard(board):
     for row in board:
@@ -81,73 +115,21 @@ def main():
         # Get the type of algorithm using the first line from the command line
         algorithm = lines[0]
         # Check if we are using the UR algorithm
-        '''#Starting code here to choose which algorithm
+        #Starting code here to choose which algorithm
         if algorithm == "UR":
-            run UR algorithm
-        if algorithm == "PMCGS":
-            run PMCGS algorithm
-        if algorithm == "UCT":
-            run UCT algorithm
-        -once these are somewhat set up, can change the below if statement to be if not UR, PMCGS, or UCT
-        '''
-        # Create a board making sure there is only 6 rows
-        board = [list(line) for line in lines[2:8]]
-        if len(board) != 6:
-            raise ValueError("You need 6 rows for the board")
-        UR_Algorithm(lines[1], board)
-        
-        if algorithm != "UR":
+            UR_Algorithm(lines[1], [list(line) for line in lines[2:8]])
+        #elif algorithm == "PMCGS":
+            #run PMCGS algorithm
+        #elif algorithm == "UCT":
+            #run UCT algorithm    
+        else:
             raise ValueError(f"Wrong Algorithm: {algorithm}")
-        # Get the symbol for the currrent player
-        current_player = lines[1]
     except Exception as e:
         print(f"Not reading file: {e}")
         sys.exit(1)
     
-    # Start array with nothing to check allowed moves; 
-    # if it is zero then its illegal
-    allowedMoves = []
-    initialBoard = board
-    print("Initial Board:") #print initial board for comparison
-    printBoard(initialBoard)
-    try:
-        # Travel through the columns to check for allowed moves
-        allowedMoves = [col for col in range(7) if board[0][col] == 'O']    #If top row is full, column cannot be added to
-        if not allowedMoves:
-            raise ValueError("No valid moves at onset, board is already full")     #Check for errors if no allowed moves
-    except Exception as e:
-        print(f"Not able to travel through array: {e}")
-        sys.exit(1)
     
-    # Check for errors if there is an index issue
-    if not allowedMoves:
-        print("Index issue moves not allowed.")
-        sys.exit(1)
     
-    # Use the UR algorithm 
-    while True:
-        allowedMoves = [col for col in range(7) if board[0][col] == 'O']    #check for allowed moves every move
-        if not allowedMoves:
-            print("Game Over: No Winner")
-            break
-        chosen_move = random.choice(allowedMoves) #Chooses random valid move 
-
-        #if selected column is not full (aka piece is dropped down correctly)
-        if dropPiece(board, chosen_move, current_player):
-            if winLogic(board, current_player):     #checks if there is a 4 in a row for the current player
-                print(f"FINAL Move Selected: {chosen_move + 1}")
-                print(f"Player {current_player} wins!")
-                printBoard(board)
-                break
-            print(f"Player {current_player} chooses column {chosen_move + 1}")
-            #printBoard(board)  #only used for testing purposes
-        else:
-            print(f"Error Coult not place piece in column {chosen_move + 1}")
-        #alternates players (always assumed R if current_player somehow isn't R or Y)
-        if current_player == 'R':
-            current_player = 'Y'
-        else:
-            current_player = "R"
 if __name__ == "__main__":
     main()
 
@@ -158,10 +140,8 @@ Things done:
     -plays a game randomly, alternating between the two players (R and Y)
     -correctly (if inefficiently) tests the board for win state, ends game and reports winner once found
         -aka Algorithm 1 works
+    -Moved UR algorithm to its own function, and implemented it in the main function
 Things to do:
-    Submission 1:
-        -Comment and clean up code
-            -Maybe get the things from main into methods (necessary for Submission 2)
     Submission 2:
         -Set up Verbose, Brief, None controls
         -Change from UR being only valid, to allowing for the 3 algorithms
