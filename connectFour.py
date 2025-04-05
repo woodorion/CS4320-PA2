fc38445389a61b2a91f383cbd341f651b70a275c
woodo <owood@miners.utep.edu>
2025-04-05 14:34:00 -0600

    Moved a few things around, started work on Algorithm 2. Also added additional board for testing algorithm 2

--- connectFour.py
+++ connectFour.py
@@ -4,13 +4,10 @@
 import sys
 import random
 
-#Uniform Random Method - takes in the current player and the game board
-#runs through board, till game is over (either win or draw)
-def UR_Algorithm(current_player, gameBoard):
+def gameSetup(gameBoard):
+    #Checks if game is set up correctly, returns initial allowed moves if valid
     if len(gameBoard) != 6:
-            raise ValueError("You need 6 rows for the board")
-    # Start array with nothing to check allowed moves; 
-    # if it is zero then its illegal
+        raise ValueError("You need 6 rows for the board")
     allowedMoves = []
     try:
         # Travel through the columns to check for allowed moves
@@ -20,46 +17,58 @@ def UR_Algorithm(current_player, gameBoard):
     except Exception as e:
         print(f"Not able to travel through array: {e}")
         sys.exit(1)
-
     # Check for errors if there is an index issue
     if not allowedMoves:
         print("Index issue moves not allowed.")
         sys.exit(1)
+    return allowedMoves
+
+def UR_Algorithm(current_player, gameBoard, verbosity):
+    #Uniform Random Method - takes in the current player, game board, and verbosity level
+    #runs through board, till game is over (either win or draw)
+    allowedMoves = gameSetup(gameBoard)    #Check if game is set up correctly
     
     # Use the UR algorithm 
     while True:
-        allowedMoves = [col for col in range(7) if gameBoard[0][col] == 'O']    #check for allowed moves every move
         if not allowedMoves:
-            print("Game Over: No Winner")
+            if verbosity != "None":
+                print("Game Over: No Winner")
             break
+        
         chosen_move = random.choice(allowedMoves) #Chooses random valid move 
 
         #if selected column is not full (aka piece is dropped down correctly)
         if dropPiece(gameBoard, chosen_move, current_player):
             if winLogic(gameBoard, current_player):     #checks if there is a 4 in a row for the current player
-                print(f"FINAL Move Selected: {chosen_move + 1}")
-                print(f"Player {current_player} wins!")
-                printBoard(gameBoard)
-                break
-            print(f"Player {current_player} chooses column {chosen_move + 1}")
-            #printBoard(board)  #only used for testing purposes
+                if verbosity != "None":
+                    print(f"FINAL Move Selected: {chosen_move + 1}")
+                    print(f"Player {current_player} wins!")
+                    printBoard(gameBoard)
+                    break
+            if verbosity == "Testing":
+                print(f"Player {current_player} chooses column {chosen_move + 1}")
+                printBoard(gameBoard)  #only used for testing purposes
+            #Update allowed moves for next player, if game is not over
+            allowedMoves = [col for col in range(7) if gameBoard[0][col] == 'O']
         else:
-            print(f"Error Coult not place piece in column {chosen_move + 1}")
+            print(f"Error Could not place piece in column {chosen_move + 1}")
         #alternates players (always assumed R if current_player somehow isn't R or Y)
-        if current_player == 'R':
-            current_player = 'Y'
-        else:
-            current_player = "R"
-#Method to print board, for testing purposes
+        current_player = 'Y' if current_player == 'R' else 'R'
+
+def PMCGS_Algorithm(current_player, gameBoard, verbosity, numSimulations):
+    allowedMoves = gameSetup(gameBoard)    #Check if game is set up correctly
+
 def printBoard(board):
+    #Method to print board, for testing/reporting purposes
     for row in board:
         print(' '.join(row))
     print('-' * 15)  # Divider between states
 
-#if valid, drops down piece to proper spot (based on board)
-#if not valid, just returns false
-#if placed in an if statement, doubles as both dropping down piece to proper place, and verifying placement validity
 def dropPiece(board, col, symbol):
+    #if valid, drops down piece to proper spot (based on board)
+    #if not valid, just returns false
+    #if placed in an if statement, doubles as both dropping down piece to proper place, and verifying placement validity
+
     # Start checking from the bottom row to the top
     for row in reversed(range(6)):
         if board[row][col] == 'O':  # Check for an empty spot
@@ -67,9 +76,9 @@ def dropPiece(board, col, symbol):
             return True
     return False  # Column is full
  
-#checks the entire board every time for possible wins for the current player
-#inefficient, but easier to implement than a local search, will likely optimize later
 def winLogic(board, symbol):
+    #checks the entire board every time for possible wins for the current player
+    #inefficient, but easier to implement than a local search, will likely optimize later
     for row in range(6):    #check horizontal lines (L to R)
         for col in range(4):
             if all(board[row][col+i] == symbol for i in range(4)):
@@ -98,7 +107,7 @@ def main():
     # For the first argument we need to get the text file
     input_file = sys.argv[1]
     verbosity = sys.argv[2]     
-    # For this argument using the UR algorithm we set it to 0
+    # Algorithm specific parameter
     algorithm_param = sys.argv[3] 
 
     # Read the file and handle errors
@@ -114,12 +123,12 @@ def main():
     try:
         # Get the type of algorithm using the first line from the command line
         algorithm = lines[0]
-        # Check if we are using the UR algorithm
-        #Starting code here to choose which algorithm
+
+        # Choose the Algorithm
         if algorithm == "UR":
-            UR_Algorithm(lines[1], [list(line) for line in lines[2:8]])
-        #elif algorithm == "PMCGS":
-            #run PMCGS algorithm
+            UR_Algorithm(lines[1], [list(line) for line in lines[2:8]], verbosity)
+        elif algorithm == "PMCGS":
+            PMCGS_Algorithm(lines[1], [list(line) for line in lines[2:8]], verbosity, algorithm_param)
         #elif algorithm == "UCT":
             #run UCT algorithm    
         else:
